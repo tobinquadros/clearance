ccd219dd1614e453b598c61b238df21e0931cc4a
Tobin Quadros <tobinquadros@gmail.com>
2017-07-17 22:09:05 -0500

    improve make and dev workflow

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
+# Update deps every time the container is run in test or dev mode
 if [[ "${ENV}" == "test" || "${ENV}" == "dev" ]]; then
   echo "executing docker-entrypoint.sh..."
-  dep ensure -v
+  dep ensure
+fi
+
+# Re-compile and run db-migrations every time the container is in dev mode
+if [[ "${ENV}" == "dev" ]]; then
+  echo "executing docker-entrypoint.sh..."
+  ./bin/install
   ./bin/db_migrations
 fi
+
+echo "finished executing docker-entrypoint.sh"
 
 exec "$@"
